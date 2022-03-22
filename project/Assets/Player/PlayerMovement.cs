123a0608454062e919e7ea427b345e9284dfcc45
Marcos Moreira <marcos.moreira@exaud.com>
2022-03-22 12:01:14 +0000

    added code comments to PlayerMovement

--- project/Assets/Player/PlayerMovement.cs
+++ project/Assets/Player/PlayerMovement.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
+    // Force applied to rigidbody on player inout
     private float flightForce;
 
     [SerializeField]
+    // Speed limit for upwards movement
     private float maxSpeed;
 
     [SerializeField]
+    // Spped limit for downwards movement
     private float minSpeed;
 
+    // player rigidbody
     private Rigidbody2D bird;
 
     void Start()
@@ -24,13 +28,17 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            // awakes the rigidbody on first player input
             if (bird.IsSleeping())
             {
                 bird.WakeUp();
             }
+
+            // applies upwards force on the rigidbody
             Fly();
         }
 
+        // Enforces the speed limits on the rigidbody
         LimitSpeed(bird.velocity);
     }
 
