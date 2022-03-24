7c2b0a0b3e4a23e6afe111f39af1733d53a81296
Marcos Moreira <marcos.moreira@exaud.com>
2022-03-24 10:27:20 +0000

    main menu | implemented quit game button

--- project/Assets/Scripts/MenuManager.cs
+++ project/Assets/Scripts/MenuManager.cs
@@ -9,4 +9,9 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene(Consts.BASE_GAME_SCENE);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
